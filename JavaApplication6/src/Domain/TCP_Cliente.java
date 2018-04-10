71ccc9ed9b2e5b553d51fed2d72b4ee8ce64d780
Stamp <Stamp@Stamp>
2018-04-10 12:57:19 -0500

    TCP server.

--- JavaApplication6/src/Domain/TCP_Cliente.java
+++ JavaApplication6/src/Domain/TCP_Cliente.java
@@ -14,7 +14,7 @@ import Controlador.ControlVistaCliente;
 import Vista.VistaCliente;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStreamReader; 
 import java.io.PrintWriter;
 import java.net.Socket;
 
